c2c527ef375cb79834014a8ca5f88fb138f591fe
Diana Alfaro <111534967+alfalfita@users.noreply.github.com>
2023-03-15 00:36:04 -0500

    feat(invalidator): funcion lambda en python + sam

---
+++ src/invalidator.py
@@ -0,0 +1,28 @@
+import boto3
+import time
+import os
+
+def handle_invalidator(event, context):
+    paths = []
+    for items in event['Records']:
+        key = items['s3']['object']['key']
+
+        if key.endswith('index.html'):
+            paths.append('/' + key[:-10])
+        paths.append('/' + key)
+
+    print('Invalidating ' + str(paths))
+
+    client = boto3.client('cloudfront')
+    batch = {
+        'Paths': {
+            'Quantity': len(paths),
+            'Items': paths
+        },
+        'CallerReference': str(time.time())
+    }
+    client.create_invalidation(
+        DistributionId=os.environ['CLOUDFRONT_DISTRIBUTION_ID'],
+        InvalidationBatch=batch,
+    )
+    return batch
